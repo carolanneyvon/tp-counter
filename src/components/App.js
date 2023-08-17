0af202b05db267c2c990c1bd27f45c1b9cc8c0c2
Carolanne Yvon <elolithia@gmail.com>
2023-08-17 12:30:47 +0200

    Gestion des événements pour chaque compteur

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,6 @@ import Data from '../services/Data';
 
 function App() {
   // Destructuring pour gérer le state
-  //const [counter, setCounter] = useState(0); // 0 est la valeur par défaut
-  //const maVariable = useState(0)[0];
-  //const setMaVariable = useState(0)[1];
   const [counters, setCounters] = useState([]);
 
   // Use Effect avec comme 2eme paramètre [] imite le hook componentDidMount
@@ -16,29 +13,37 @@ function App() {
     console.log(`Dans useEffect`);
     // Appel du service Data
     (async () => {
-      // const counters = await Data.loadCounters();
       setCounters(await Data.loadCounters())
       console.log(`counters : `, counters);
     })();
   }, []);
 
-  const increment = () => {
-    //setCounters(counters + 1)
+  // Incrémente uniquement le compteur dont on a l'id
+  const increment = (id) => {
+    console.log(`Dans increment`);
+    setCounters(counters => counters.map((counter) => {
+      if (id === counter.id) counter.value++;
+      return counter;
+    }));
+  }
+  // Decremente uniquement le compteur dont on a l'id
+  const decrement = (id) => {
+    console.log(`Dans decrement`);
+    setCounters (counters => counters.map((counter) => {
+      if (id === counter.id) counter.value --;
+      return counter;
+    }));
+  }
+
+  const incrementAll = () => {
     // Pour agir sur tous les compteurs
     const copy_counters = counters.map(counter => {
       counter.value ++;
       return counter;
     })
-    // autre solution avec le spread operator [...xxx]
-    // const copy_counters2 = [...counters];
-    // copy_counters2.forEach(counter => {
-    //   counter.value ++;
-    // })
     setCounters (counters => copy_counters);
   }
-  const decrement = () => {
-    //setCounters(counters - 1)
-    // Pour agir sur tous les compteurs
+  const decrementAll = () => {
     const copy_counters = counters.map(counter => {
       counter.value --;
       return counter;
@@ -50,13 +55,21 @@ function App() {
     <div className="App container">
       <h1 className='pb-3'>Compteur</h1>
       <button
-        onClick={decrement}
-        className="btn btn-warning me-3">Decrémenter</button>
-      {/* <Counter counter={counter} /> */}
+        onClick={decrementAll}
+        className="btn btn-warning me-3">Decrémenter
+      </button>
       <button
-        onClick={increment}
-        className="btn btn-warning ms-3">Incrémenter</button>
-      {counters.map(counter => <Counter key={counter.id} counter={counter}/>)}
+        onClick={incrementAll}
+        className="btn btn-warning ms-3">Incrémenter
+      </button>
+
+      {counters.map(counter => 
+        <Counter 
+          key={counter.id} 
+          counter={counter}
+          onIncrement={increment}
+          onDecrement={decrement}
+        />)}
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,19 @@
 const Counter = (props) => {
   return (
     <div className="my-2">
-      <button className="btn btn-success">{props.counter.value}</button>
+        <button
+          onClick={()=>{
+            props.onDecrement(props.counter.id)
+          }}
+          className="btn btn-warning me-3">Decrémenter
+        </button>
+        <button
+          onClick={ () => {
+            props.onIncrement(props.counter.id);
+          }}
+          className="btn btn-warning me-3">Incrémenter
+        </button>
+        <button className="btn btn-success">{props.counter.value}</button>
     </div>
   );
 }
